5ae0c5c216497b7555b0d3ce83849b874db5dc86
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-08 11:11:01 -0500

    💡 Comment on Constantes

--- src/02_Constantes/main.go
+++ src/02_Constantes/main.go
@@ -6,5 +6,6 @@ func main() {
 	// Declaración de una constante
 	const pi float64 = 3.1415
 
-	fmt.Println("pi = ", pi)
+	// Println es Usado para que al imprimir haga un salto de linea al final
+	fmt.Println("El valor de pi es", pi)
 }
