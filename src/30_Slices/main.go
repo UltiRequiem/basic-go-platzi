64f30f1cb44fa06702a7b57c80cee56094d25ae1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-12 22:37:43 -0500

    🎨 Sprint

--- src/30_Slices/main.go
+++ src/30_Slices/main.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	// Primer Slice
 	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println("Los valores del slice son:", slice, "\n", "Hay", len(slice), "elementos.", "\n", "La capacidad maxima del slice es", cap(slice), ".")
+	var message string = fmt.Sprint("Los valores del slice son:", slice, "\n", "Hay", len(slice), "elementos.", "\n", "La capacidad maxima del slice es", cap(slice), ".")
+	fmt.Println(message)
 }
